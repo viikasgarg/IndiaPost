f15a84bc8b47855924845958607fb8af66c71736
vikas kumar <viikasgarg@gmail.com>
2016-07-03 15:31:06 +0530

    Url configured

--- indiapost/urls.py
+++ indiapost/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from postalcode import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', views.home, name='home')
+    url(r'^$', views.home, name='home')
 
 ]
