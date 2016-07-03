ec24dc8001ecb4dd20f6ce045c47cdc8ff35e611
vikas kumar <viikasgarg@gmail.com>
2016-07-03 15:04:24 +0530

    database config issue

--- indiapost/settings.py
+++ indiapost/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = 'indiapost.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    '''
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    '''
 }
+'''
+'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+'''
 
 
 
