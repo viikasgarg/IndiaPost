41d14bc48bdfaed943a127344ce9e8e699d3e50a
vikas kumar <viikasgarg@gmail.com>
2016-07-03 14:06:32 +0530

    Added ProcFile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn indiapost.wsgi --log-file -
--- indiapost/wsgi.py
+++ indiapost/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
+
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "indiapost.settings")
 
-application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
